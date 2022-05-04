26ab8bcc1c4b2bcf25adde046073e0e629858a70
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2022-05-04 16:25:45 -0700

    added portfolio main codes

---
+++ src/components/PortfolioMain.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import NavBar from './NavBar';
+import Resume from './pages/Resume';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
+export default function PortfolioMain() {
+  const [currentPage, setCurrentPage] = useState('About');
+  const renderPage = () => {
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+ 
+  return (
+    <div>
+      <NavBar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      {renderPage()}
+    </div>
+  );
+}
