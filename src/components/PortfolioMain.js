b550b6f1986964418ab13f22a57966de10f4f929
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2025-02-06 14:32:36 -0800

    issue stemming from gh - attempt to create work around via 404 html file

--- src/components/PortfolioMain.js
+++ src/components/PortfolioMain.js
@@ -1,36 +1,3 @@
-// import React, { useState } from "react";
-// import NavBar from "./NavBar";
-// import Resume from "./pages/Resume";
-// import About from "./pages/About";
-// import Portfolio from "./pages/Portfolio";
-// import Contact from "./pages/Contact";
-
-// export default function PortfolioMain() {
-//   const [currentPage, setCurrentPage] = useState("About");
-//   const renderPage = () => {
-//     if (currentPage === "Resume") {
-//       return <Resume />;
-//     }
-//     if (currentPage === "About") {
-//       return <About />;
-//     }
-//     // if (currentPage === 'Edu') {
-//     //   return <Edu />;
-//     // }
-//     if (currentPage === "Portfolio") {
-//       return <Portfolio />;
-//     }
-//     return <Contact />;
-//   };
-
-//   return (
-//     <div>
-//       <NavBar currentPage={currentPage} setCurrentPage={setCurrentPage} />
-//       {renderPage()}
-//     </div>
-//   );
-// }
-
 import React, { useState, useEffect } from "react";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
@@ -42,20 +9,19 @@ function PortfolioMain() {
   const [currentPage, setCurrentPage] = useState("About");
 
   // useEffect(() => {
-  //   // Read the hash from the URL (if it exists)
-  //   const hash = window.location.hash.replace("#", "").toLowerCase();
+  //   const path = window.location.pathname.replace("/jasonyoo-fullstack-portfolio/", "").toLowerCase();
   //   const validPages = ["about", "portfolio", "contact", "resume"];
-
-  //   if (validPages.includes(hash)) {
-  //     setCurrentPage(hash.charAt(0).toUpperCase() + hash.slice(1)); // Convert "portfolio" -> "Portfolio"
+  
+  //   if (validPages.includes(path)) {
+  //     setCurrentPage(path.charAt(0).toUpperCase() + path.slice(1));
   //   } else {
-  //     // Default to About if no valid hash
   //     setCurrentPage("About");
   //   }
-  // }, [window.location.hash]);
+  // }, []);
 
   useEffect(() => {
-    const path = window.location.pathname.replace("/jasonyoo-fullstack-portfolio/", "").toLowerCase();
+    const basePath = "/jasonyoo-fullstack-portfolio";
+    const path = window.location.hash.replace("#", "").toLowerCase();
     const validPages = ["about", "portfolio", "contact", "resume"];
   
     if (validPages.includes(path)) {
